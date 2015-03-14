b9c8911278f96839e1c713fa37daf513ed40ebab
David Leonard <sephirothcloud1025@yahoo.com>
2015-03-13 20:58:54 -0400

    Using Outlets to change text field values within iOS application

--- Example Name/Example Name/ViewController.swift
+++ Example Name/Example Name/ViewController.swift
@@ -10,9 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        myLabel.text = "It Worked!";
+        
+        println("Hello World!");
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
