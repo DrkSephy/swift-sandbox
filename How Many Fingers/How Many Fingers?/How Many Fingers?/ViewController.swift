2fa92c86f579066673a3f5f0e97eff1890ff84fa
David Leonard <sephirothcloud1025@yahoo.com>
2015-03-14 12:30:00 -0400

    Intermediate work on How Many Fingers application

--- How Many Fingers/How Many Fingers?/How Many Fingers?/ViewController.swift
+++ How Many Fingers/How Many Fingers?/How Many Fingers?/ViewController.swift
@@ -10,6 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+   
+    @IBOutlet weak var guess: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBAction func guessButton(sender: AnyObject) {
+        // Generate random number between 1 and 5
+        var randomNumber = arc4random_uniform(6);
+        var guessInt = guess.text.toInt();
+        if (guessInt != nil) {
+            
+        } else {
+            resultLabel.text = "Please enter a number 0-5";
+        }
+        
+        
+        println(guess.text);
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
