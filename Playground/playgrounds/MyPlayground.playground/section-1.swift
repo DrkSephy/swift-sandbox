543ad39e61180cc30d6f4a52291bdfe85049e5b6
David Leonard <sephirothcloud1025@yahoo.com>
2015-03-14 09:59:52 -0400

    Adding playground files

--- Playground/playgrounds/MyPlayground.playground/section-1.swift
+++ Playground/playgrounds/MyPlayground.playground/section-1.swift
@@ -2,4 +2,36 @@
 
 import UIKit
 
-var str = "Hello, playground"
+/* Strings*/
+
+// Create a variable
+var str = "Hello, playground";
+
+// Update the value of `str`
+str = "Goodbye";
+
+// Create new variable, with type declared as a String
+// NOTE: Swift uses TYPE INFERENCE to figure out what type a variable is without giving it
+// an explicit type.
+var name:String = "David";
+
+// Change name value
+name = "Lisa";
+
+var unknownString:String
+unknownString = "3";
+
+/* Integers */
+
+// Integers are useful when we know our input will only be whole numbers
+var int = 4;
+3 * int;
+
+var a:Int = 5;
+var b:Int = a * 5;
+
+// Because we started with an integer 5, Swift infers that C will be an integer, 
+// resulting in 2 and truncating the decimal.
+// To fix this, we use a DOUBLE.
+var c:Double = 5 / 2;
+c * 2;
