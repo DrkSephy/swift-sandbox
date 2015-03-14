98c6b65984bddbd8e24ddc453c2cd25afaa17fc9
David Leonard <sephirothcloud1025@yahoo.com>
2015-03-13 22:09:57 -0400

    Ignoring .DS_Store files

---
+++ catYears/Cat Years/Cat Years/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Cat Years
+//
+//  Created by David Leonard on 3/13/15.
+//  Copyright (c) 2015 David Leonard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
