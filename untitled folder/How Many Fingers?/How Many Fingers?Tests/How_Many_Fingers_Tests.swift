2fe6454c10f15e2711decf59d7edcff4afb066ea
David Leonard <sephirothcloud1025@yahoo.com>
2015-03-14 12:06:42 -0400

    Initial commit for How Many Fingers project

---
+++ untitled folder/How Many Fingers?/How Many Fingers?Tests/How_Many_Fingers_Tests.swift
@@ -0,0 +1,36 @@
+//
+//  How_Many_Fingers_Tests.swift
+//  How Many Fingers?Tests
+//
+//  Created by David Leonard on 3/14/15.
+//  Copyright (c) 2015 David Leonard. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class How_Many_Fingers_Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
