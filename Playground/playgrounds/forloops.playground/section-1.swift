830009cdbe441c9115bdd5fdfd6ce81923420ef8
David Leonard <sephirothcloud1025@yahoo.com>
2015-03-14 12:51:24 -0400

    Adding for loop playground

---
+++ Playground/playgrounds/forloops.playground/section-1.swift
@@ -0,0 +1,14 @@
+// David Leonard
+// Playground for experimenting with for loops
+
+import UIKit
+
+/* Challenge: Display 3, 5, 7 */
+for var i = 3; i < 8; i = i + 2 {
+    println(i);
+}
+
+for var j = 30; j > 8; j = j - 2 {
+    println(j);
+}
+
