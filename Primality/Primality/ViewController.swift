4a5352472d900fb6ef0a9535cdf5da2374ef4fc5
David Leonard <sephirothcloud1025@yahoo.com>
2015-03-15 17:48:02 -0400

    Adding primality base

--- Primality/Primality/ViewController.swift
+++ Primality/Primality/ViewController.swift
@@ -10,6 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    @IBOutlet weak var number: UITextField!
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBAction func buttonPressed(sender: AnyObject) {
+        // Convert input to integer
+        var numberInt = number.text.toInt();
+        
+        // Use statement to make sure numberInt is not nil
+        if numberInt != nil {
+            // Do primality check
+            
+        } else {
+            resultLabel.text = "Please enter a number!";
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
