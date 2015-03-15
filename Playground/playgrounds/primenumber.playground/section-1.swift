9e1787d8b11512f77c5dd5df1b07d9f34900359a
David Leonard <sephirothcloud1025@yahoo.com>
2015-03-15 16:56:45 -0400

    Wrote code to determine if a number is prime

--- Playground/playgrounds/primenumber.playground/section-1.swift
+++ Playground/playgrounds/primenumber.playground/section-1.swift
@@ -3,3 +3,22 @@
 
 import UIKit
 
+// Test number
+var number = 3;
+// Begin with assuming that a number is prime
+var isPrime = true;
+
+if number == 1 {
+    isPrime = false;
+}
+
+if number != 2 && number != 1 {
+    for var i = 2; i < number; i++ {
+        if number % i == 0 {
+            isPrime = false
+        }
+    }
+}
+
+println(isPrime);
+    
