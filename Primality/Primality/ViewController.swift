7870ab014b6804dec7606c57a2742dce146ed0e3
David Leonard <sephirothcloud1025@yahoo.com>
2015-03-15 18:02:33 -0400

    Finished primality app

--- Primality/Primality/ViewController.swift
+++ Primality/Primality/ViewController.swift
@@ -23,6 +23,27 @@ class ViewController: UIViewController {
         // Use statement to make sure numberInt is not nil
         if numberInt != nil {
             // Do primality check
+            // Begin with assuming that a number is prime
+            var isPrime = true;
+            var unwrappedNumber = numberInt!;
+            
+            if unwrappedNumber == 1 {
+                isPrime = false;
+            }
+            
+            if unwrappedNumber != 2 && unwrappedNumber != 1 {
+                for var i = 2; i < unwrappedNumber; i++ {
+                    if unwrappedNumber % i == 0 {
+                        isPrime = false
+                    }
+                }
+            }
+            
+            if isPrime == true {
+                resultLabel.text = "\(unwrappedNumber) is prime.";
+            } else {
+                resultLabel.text = "\(unwrappedNumber) is not prime.";
+            }
             
         } else {
             resultLabel.text = "Please enter a number!";
