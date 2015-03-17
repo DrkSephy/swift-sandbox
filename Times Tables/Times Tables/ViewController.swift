fc4be1181197341ddef01545d6ae384ea252cf5c
David Leonard <sephirothcloud1025@yahoo.com>
2015-03-16 21:25:22 -0400

    Setting up base times table

--- Times Tables/Times Tables/ViewController.swift
+++ Times Tables/Times Tables/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate {
 
     @IBOutlet weak var sliderValue: UISlider!
     @IBAction func sliderMoved(sender: AnyObject) {
@@ -17,6 +17,18 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20;
+    }
+    
+ 
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell");
+        let timesTable = Int(sliderValue.value * 20);
+        cell.textLabel?.text = String(timesTable * indexPath.row);
+        return cell; 
     }
 
     override func didReceiveMemoryWarning() {
