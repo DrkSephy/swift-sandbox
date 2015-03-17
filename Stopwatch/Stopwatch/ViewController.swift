f66011773422f7ec909d3608ccece52e00216051
David Leonard <sephirothcloud1025@yahoo.com>
2015-03-16 20:47:33 -0400

    Implemented counting feature of stopwatch

--- Stopwatch/Stopwatch/ViewController.swift
+++ Stopwatch/Stopwatch/ViewController.swift
@@ -10,6 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // Create timer object
+    var timer = NSTimer();
+    
+    // Create timer count variable
+    var count = 0;
+    // Create update time method
+    func updateTime(){
+        count++;
+        time.text = "\(count)";
+    }
+    
+    @IBOutlet weak var time: UILabel!
+    
+    @IBAction func pause(sender: AnyObject) {
+    
+    }
+    
+    
+    @IBAction func stop(sender: AnyObject) {
+    
+    }
+    
+    /* Play button logic */
+    @IBAction func play(sender: AnyObject) {
+        timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("updateTime"), userInfo: nil, repeats: true);
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
