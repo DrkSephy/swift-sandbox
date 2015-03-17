719e7d792c8e498240536c557885a86c8c845719
David Leonard <sephirothcloud1025@yahoo.com>
2015-03-16 21:18:17 -0400

    Writing logging action for slider

--- Times Tables/Times Tables/ViewController.swift
+++ Times Tables/Times Tables/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sliderValue: UISlider!
+    @IBAction func sliderMoved(sender: AnyObject) {
+        println(sliderValue);
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
