c59a70b39ca518419ee82525fbe2fdb4c654a858
David Leonard <sephirothcloud1025@yahoo.com>
2015-03-18 13:21:57 -0400

    Downloading content from Stack Overflow

--- Downloading Web Content/Downloading Web Content/ViewController.swift
+++ Downloading Web Content/Downloading Web Content/ViewController.swift
@@ -13,6 +13,23 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Define url which we want to fetch data from
+        let url = NSURL(string: "http://www.stackoverflow.com");
+        // Define task for getting data from a URL
+        let task = NSURLSession.sharedSession().dataTaskWithURL(url!) {
+            // data is returns through the following three parameters
+            // NOTE: This is also known as a closure!
+            (data, response, error) in
+            // Check if there was an error with fetching the data
+            if error == nil {
+                println(data);
+            }
+            
+        }
+        // Run the task
+        task.resume();
+        
     }
 
     override func didReceiveMemoryWarning() {
