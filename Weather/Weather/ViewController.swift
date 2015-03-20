48b8e116fd5ea64855f210b6538753eab6bd91ac
David Leonard <sephirothcloud1025@yahoo.com>
2015-03-19 22:43:46 -0400

    Grabbing weather content from weather-forecast.com

--- Weather/Weather/ViewController.swift
+++ Weather/Weather/ViewController.swift
@@ -10,9 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var userCity: UITextField!
+    
+    @IBAction func findWeather(sender: AnyObject) {
+        
+    }
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    func showError() {
+        resultLabel.text = "Was not able to find weather for " + userCity.text + ". Please try again.";
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        var url = NSURL(string: "http://www.weather-forecast.com/locations/London/forecasts/latest");
+        // Check URL to make sure it exists
+        if url != nil {
+            let task = NSURLSession.sharedSession().dataTaskWithURL(url!, completionHandler: {(data, response, error) -> Void in
+                var urlError = false;
+                if error == nil {
+                    var urlContent = NSString(data: data, encoding: NSUTF8StringEncoding);
+                    println(urlContent);
+                } else {
+                    urlError = true;
+                }
+                
+                if urlError == true {
+                    self.showError();
+                }
+            
+            });
+            task.resume();
+        } else {
+            showError();
+        }
     }
 
     override func didReceiveMemoryWarning() {
