5237e8fe29f6b12cc3199a69fe39bca0111e1707
David Leonard <sephirothcloud1025@yahoo.com>
2015-03-20 00:03:38 -0400

    Manually setting a new frame on click

--- Animations/Animations/ViewController.swift
+++ Animations/Animations/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var alienImage: UIImageView!
+    
+    @IBAction func updateImage(sender: AnyObject) {
+        // Update image on click
+        alienImage.image = UIImage(named: "frame2.png");
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
