804da2e14174f012d891ce5915e876a50539c428
David Leonard <sephirothcloud1025@yahoo.com>
2015-03-21 19:33:06 -0400

    Setting up secondary view controller for tableView

--- Memorable Places/Memorable Places/ViewController.swift
+++ Memorable Places/Memorable Places/ViewController.swift
@@ -7,9 +7,15 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+    
+    
+    @IBOutlet weak var map: MKMapView!;
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
