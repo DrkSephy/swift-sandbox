593b58188d6cf16035436100192280e7e70ed086
David Leonard <sephirothcloud1025@yahoo.com>
2015-03-20 22:30:58 -0400

    Checking for winning combinations

--- Tic Tac Toe/Tic Tac Toe/ViewController.swift
+++ Tic Tac Toe/Tic Tac Toe/ViewController.swift
@@ -17,6 +17,14 @@ class ViewController: UIViewController {
     // 0 = empty
     var gameState = [0, 0, 0, 0, 0, 0, 0, 0, 0];
     
+    // Winning conditions
+    // [0, 1, 2]: top line
+    // [3, 4, 5]: middle horizontal line
+    // [6, 7, 8]: bottom horizontal line
+    // ...
+    // ...
+    var winningCombinations = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
+    
     @IBOutlet weak var button: UIButton!
 
     @IBAction func buttonPressed(sender: AnyObject) {
@@ -33,6 +41,13 @@ class ViewController: UIViewController {
             // Display image for normal state
             // We use sender to indicate the UI element that was tapped
             sender.setImage(image, forState: .Normal);
+            // Loop through winning combinations
+            for combination in winningCombinations {
+                // Manual check for a winner on the top row
+                if gameState[combination[0]] != 0 && gameState[combination[0]] == gameState[combination[1]] && gameState[combination[1]] == gameState[combination[2]] {
+                    println("We have a winner!");
+                }
+            }
         }
 
     }
