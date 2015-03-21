02debe8b3b1d9f8f8f9075ffe6c6e1dc272cf802
David Leonard <sephirothcloud1025@yahoo.com>
2015-03-21 18:46:55 -0400

    Finishing application

--- Where Am I/Where Am I/ViewController.swift
+++ Where Am I/Where Am I/ViewController.swift
@@ -31,6 +31,28 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     
     func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
         println(locations);
+        var userLocation: CLLocation = locations[0] as CLLocation;
+        self.latitudeLabel.text = "\(userLocation.coordinate.latitude)";
+        self.longitudeLabel.text = "\(userLocation.coordinate.longitude)";
+        self.courseLabel.text = "\(userLocation.course)";
+        self.speedLabel.text = "\(userLocation.speed)";
+        self.altitudeLabel.text = "\(userLocation.altitude)";
+        // reverseGeocodeLocation takes a lat/lon pair and gets the address
+        CLGeocoder().reverseGeocodeLocation(userLocation, completionHandler: { (placemarks, error) -> Void in
+            if (error != nil){
+                println(error);
+            } else {
+                // Create a new placemark
+                if let p = CLPlacemark(placemark: placemarks?[0] as CLPlacemark) {
+                    var subThoroughfare: String = "";
+                    if p.subThoroughfare != nil {
+                        subThoroughfare = p.subThoroughfare;
+                    } else {
+                        self.addressLabel.text = "\(subThoroughfare) \(p.thoroughfare) \n \(p.subLocality) \n \(p.subAdministrativeArea) \n \(p.postalCode) \n \(p.country)"
+                    }
+                }
+            }
+        })
     }
 
     override func didReceiveMemoryWarning() {
