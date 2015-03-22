a4fd548e41d74fd3f3fd070219934bf40f69fbcf
David Leonard <sephirothcloud1025@yahoo.com>
2015-03-21 23:27:06 -0400

    Creating new demo app, detecting right swipes

---
+++ Shake and Swipe/Shake and Swipe/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  Shake and Swipe
+//
+//  Created by David Leonard on 3/21/15.
+//  Copyright (c) 2015 David Leonard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        var swipeRight = UISwipeGestureRecognizer(target: self, action: "swiped:");
+        swipeRight.direction = UISwipeGestureRecognizerDirection.Right;
+        self.view.addGestureRecognizer(swipeRight);
+    }
+    
+    func swiped(gesture: UIGestureRecognizer) {
+        if let swipeGesture = gesture as? UISwipeGestureRecognizer {
+            switch swipeGesture.direction {
+            case UISwipeGestureRecognizerDirection.Right:
+                println("User swiped right");
+            default:
+                break;
+            }
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
