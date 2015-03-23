0e66c14ab5adbb790e8a2ebde663d1ff239886f7
David Leonard <sephirothcloud1025@yahoo.com>
2015-03-22 22:09:24 -0400

    Interacting with database

--- Core Data Demo/Core Data Demo/ViewController.swift
+++ Core Data Demo/Core Data Demo/ViewController.swift
@@ -7,12 +7,22 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        var appDel: AppDelegate = UIApplication.sharedApplication().delegate as AppDelegate;
+        // Context provides a framework for accessing Core Data
+        var context:NSManagedObjectContext = appDel.managedObjectContext!
+        // An Entity is a table
+        var newUser = NSEntityDescription.insertNewObjectForEntityForName("Users", inManagedObjectContext: context) as NSManagedObject;
+        
+        // Create a new instance in the database
+        newUser.setValue("David", forKey: "username")
+        newUser.setValue("pass", forKey: "password");
+        context.save(nil);
     }
 
     override func didReceiveMemoryWarning() {
