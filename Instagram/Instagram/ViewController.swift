f73bff3b5e3f1af77360da6ff5e1665e119f3cf1
David Leonard <sephirothcloud1025@yahoo.com>
2015-03-23 18:11:14 -0400

    Setting up user interface for sign up screen

--- Instagram/Instagram/ViewController.swift
+++ Instagram/Instagram/ViewController.swift
@@ -12,56 +12,6 @@ import UIKit
 // UIImagePickerControllerDelegate allows the user to select an image
 class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
     
-    var activityIndicator: UIActivityIndicatorView = UIActivityIndicatorView();
-    
-    @IBOutlet weak var pickedImage: UIImageView!
-    // This image will prevent the view from closing when the user selects an image, because we overrode the method
-    func imagePickerController(picker: UIImagePickerController!, didFinishPickingImage image: UIImage!, editingInfo: [NSObject : AnyObject]!) {
-        println("Image Selected");
-        // To close the controller after the user selects an image, we need to do the following:
-        self.dismissViewControllerAnimated(true, completion: nil);
-        pickedImage.image = image; // Display the picked image
-    }
-    
-    // Pick an image
-    @IBAction func pickImage(sender: AnyObject) {
-        var image = UIImagePickerController();
-        // Set properties for ImagePickerController
-        image.delegate = self;
-        // The camera roll...
-        image.sourceType = UIImagePickerControllerSourceType.PhotoLibrary;
-        // Allows the user to pan/crop before the image is used in the app
-        image.allowsEditing = false;
-        // Display the picker controller
-        self.presentViewController(image, animated: true, completion: nil);
-    }
-    
-    
-    // Create a spinning dial on the app while user is waiting
-    @IBAction func pause(sender: AnyObject) {
-        activityIndicator = UIActivityIndicatorView(frame: CGRectMake(0, 0, 50, 50));
-        activityIndicator.center = self.view.center; // Center spinner on screen
-        activityIndicator.hidesWhenStopped = true; // Make it disappear when app is stopped
-        activityIndicator.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.Gray; // Color the spinner
-        view.addSubview(activityIndicator); // Add spinner
-        activityIndicator.startAnimating();
-        // Disable the app when paused, user cannot click anything
-        // UIApplication.sharedApplication().beginIgnoringInteractionEvents();
-    }
-    
-    @IBAction func restore(sender: AnyObject) {
-        activityIndicator.stopAnimating();
-        // UIApplication.sharedApplication().endIgnoringInteractionEvents(); // Restart the app
-    }
-    
-    @IBAction func createAlert(sender: AnyObject) {
-        var alert = UIAlertController(title: "Hey There!", message: "Are you sure?", preferredStyle: UIAlertControllerStyle.Alert);
-        alert.addAction(UIAlertAction(title: "OK", style: .Default, handler: { action in
-            self.dismissViewControllerAnimated(true, completion: nil);
-        }));
-        self.presentViewController(alert, animated: true, completion: nil);
-    }
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
