3c9b6ea6fb4d888b13fcd84254758a94c1a4f70a
David Leonard <sephirothcloud1025@yahoo.com>
2015-03-23 15:46:11 -0400

    Grabbing JSON from API

---
+++ JSON Demo/JSON Demo/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  JSON Demo
+//
+//  Created by David Leonard on 3/23/15.
+//  Copyright (c) 2015 David Leonard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // URL to get JSON from
+        let urlPath = "http://www.telize.com/geoip";
+        let url = NSURL(string: urlPath);
+        
+        let session = NSURLSession.sharedSession();
+        let task = session.dataTaskWithURL(url!, completionHandler: {data, response, error -> Void in
+            if error != nil {
+                println(error);
+            } else {
+                println(data);
+            }
+        
+        });
+        task.resume();
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
