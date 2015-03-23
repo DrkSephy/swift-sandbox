52085e93dfa5e52c38e9c5fc46623a8964a2006c
David Leonard <sephirothcloud1025@yahoo.com>
2015-03-23 18:05:54 -0400

    Moving ParseID to appDelegate, which is run once on app load

--- Instagram/Instagram/AppDelegate.swift
+++ Instagram/Instagram/AppDelegate.swift
@@ -16,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        Parse.setApplicationId("JfNgWLfYn0peGb6WBOLbjrFLK9wrOqMMdOY6b40n", clientKey: "EeeBNSR3ZwWy26DEXp1H2j4oa2vEVKmKe4jwAAnC");
         return true
     }
 
--- Instagram/Instagram/ViewController.swift
+++ Instagram/Instagram/ViewController.swift
@@ -65,43 +65,9 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        Parse.setApplicationId("JfNgWLfYn0peGb6WBOLbjrFLK9wrOqMMdOY6b40n", clientKey: "EeeBNSR3ZwWy26DEXp1H2j4oa2vEVKmKe4jwAAnC");
         
-        // Anything with a className of score will have certain characteristics
-        // score is now an object - OOP
-        /*
-        var score = PFObject(className: "score");
-        score.setObject("David", forKey: "name"); // Added a new variable to score
-        score.setObject(95, forKey: "number");
-        score.saveInBackgroundWithBlock { // We needed to bridge the bolts framework to get this to work
-            (success: Bool!, error: NSError!) -> Void in
-            if success == true {
-                println("Score created with ID: \(score.objectId)");
-            } else {
-                println(error);
-            }
-        };
-
         
-        // query the "score" class
-        var query = PFQuery(className: "score");
-        query.getObjectInBackgroundWithId("3YxZuFD1GO") {
-            (score: PFObject!, error: NSError!) -> Void in
-                if error == nil  {
-                    // Get information for one entry
-                    // We can also use score["name"] = "Lisa"
-                    // println(score.objectForKey("name"));
-                    score["name"] = "Lisa";
-                    score["number"] = 137;
-                    score.save();
-                } else {
-                    println(error);
-                }
-        };
-        */
         
-        // Start importing image from User's Camera role
-
         
     }
     
