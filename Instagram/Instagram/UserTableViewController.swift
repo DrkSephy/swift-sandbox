7b536475c8019de0750ec4928c006935292b1945
David Leonard <sephirothcloud1025@yahoo.com>
2015-03-24 22:40:34 -0400

    Refactoring adding users into updateUsers method

--- Instagram/Instagram/UserTableViewController.swift
+++ Instagram/Instagram/UserTableViewController.swift
@@ -10,72 +10,17 @@ import UIKit
 
 class UserTableViewController: UITableViewController {
     
-    var users = [""]
-    var following = [Bool]()
+    var users = [""];
+    var following = [Bool]();
     var refresher: UIRefreshControl!; // Pull to refresh
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        println(PFUser.currentUser())
+        println(PFUser.currentUser());
         
-        var query = PFUser.query()
-        
-        query.findObjectsInBackgroundWithBlock({ (objects: [AnyObject]!, error: NSError!) -> Void in
-        
-            self.users.removeAll(keepCapacity: true)
-            
-            for object in objects {
-                
-                var user:PFUser = object as PFUser
-                
-                var isFollowing:Bool
-                
-                if user.username != PFUser.currentUser().username {
-                
-                    self.users.append(user.username)
-                    
-                    isFollowing = false
-                    
-                    var query = PFQuery(className:"followers")
-                    query.whereKey("follower", equalTo:PFUser.currentUser().username)
-                    query.whereKey("following", equalTo:user.username)
-                    query.findObjectsInBackgroundWithBlock {
-                        (objects: [AnyObject]!, error: NSError!) -> Void in
-                        if error == nil {
-                            
-                            for object in objects {
-                                
-                                isFollowing = true
-                                
-                            }
-                            
-                            self.following.append(isFollowing)
-                            
-                            self.tableView.reloadData()
-                            
-                        } else {
-                            // Log details of the failure
-                            println(error)
-                        }
-                    }
-                    
-                }
-                
-                
-                
-                
-                
-               
-
-                
-                
-            }
-            
-            
-        
-        })
+        updateUsers();
         
         // Pull to refresh code
         refresher = UIRefreshControl();
@@ -87,22 +32,76 @@ class UserTableViewController: UITableViewController {
         
     }
     
+    
+    func updateUsers() {
+        
+        var query = PFUser.query();
+        
+        query.findObjectsInBackgroundWithBlock({ (objects: [AnyObject]!, error: NSError!) -> Void in
+            
+            self.users.removeAll(keepCapacity: true);
+            
+            for object in objects {
+                
+                var user:PFUser = object as PFUser;
+                
+                var isFollowing:Bool;
+                
+                if user.username != PFUser.currentUser().username {
+                    
+                    self.users.append(user.username);
+                    
+                    isFollowing = false;
+                    
+                    var query = PFQuery(className:"followers")
+                    query.whereKey("follower", equalTo:PFUser.currentUser().username);
+                    query.whereKey("following", equalTo:user.username);
+                    query.findObjectsInBackgroundWithBlock {
+                        (objects: [AnyObject]!, error: NSError!) -> Void in
+                        if error == nil {
+                            
+                            for object in objects {
+                                
+                                isFollowing = true;
+                                
+                            }
+                            
+                            self.following.append(isFollowing);
+                            
+                            self.tableView.reloadData();
+                            
+                        } else {
+                            // Log details of the failure
+                            println(error);
+                        }
+                        // Stop refreshing animation when it is finished
+                        self.refresher.endRefreshing();
+                    }
+                    
+                }
+            }
+        });
+
+        
+    }
+    
     func refresh() {
         println("refreshed");
+        updateUsers();
     }
     
     override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
+        super.didReceiveMemoryWarning();
         // Dispose of any resources that can be recreated.
     }
     
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 1
+        return 1;
     }
     
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        println(following)
-        return users.count
+        println(following);
+        return users.count;
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
@@ -112,42 +111,42 @@ class UserTableViewController: UITableViewController {
         
             if following[indexPath.row] == true {
             
-                cell.accessoryType = UITableViewCellAccessoryType.Checkmark
+                cell.accessoryType = UITableViewCellAccessoryType.Checkmark;
             
             }
             
         }
         
-        cell.textLabel?.text = users[indexPath.row]
+        cell.textLabel?.text = users[indexPath.row];
         
-        return cell
+        return cell;
         
     }
     
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         
         
-        var cell:UITableViewCell = tableView.cellForRowAtIndexPath(indexPath)!
+        var cell:UITableViewCell = tableView.cellForRowAtIndexPath(indexPath)!;
         
         if cell.accessoryType == UITableViewCellAccessoryType.Checkmark {
             
-            cell.accessoryType = UITableViewCellAccessoryType.None
+            cell.accessoryType = UITableViewCellAccessoryType.None;
             
             var query = PFQuery(className:"followers")
-            query.whereKey("follower", equalTo:PFUser.currentUser().username)
-            query.whereKey("following", equalTo:cell.textLabel?.text)
+            query.whereKey("follower", equalTo:PFUser.currentUser().username);
+            query.whereKey("following", equalTo:cell.textLabel?.text);
             query.findObjectsInBackgroundWithBlock {
                 (objects: [AnyObject]!, error: NSError!) -> Void in
                 if error == nil {
                     
                     for object in objects {
                         
-                        object.deleteInBackground()
+                        object.deleteInBackground();
                         
                     }
                 } else {
                     // Log details of the failure
-                    println(error)
+                    println(error);
                 }
             }
             
@@ -155,11 +154,11 @@ class UserTableViewController: UITableViewController {
         
             cell.accessoryType = UITableViewCellAccessoryType.Checkmark
             
-            var following = PFObject(className: "followers")
-            following["following"] = cell.textLabel?.text
-            following["follower"] = PFUser.currentUser().username
+            var following = PFObject(className: "followers");
+            following["following"] = cell.textLabel?.text;
+            following["follower"] = PFUser.currentUser().username;
             
-            following.saveInBackground()
+            following.saveInBackground();
             
         }
         
