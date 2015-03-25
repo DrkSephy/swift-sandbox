4d5b5a36d63a1c137b4ea70cbd949f2ac6787f10
David Leonard <sephirothcloud1025@yahoo.com>
2015-03-25 16:22:56 -0400

    Setting up Facebook Login for Tinder clone

---
+++ Tinder/Tinder/AppDelegate.swift
@@ -0,0 +1,80 @@
+//
+//  AppDelegate.swift
+//  Tinder
+//
+//  Created by David Leonard on 03/25/15.
+//  Copyright (c) 2015 DrkSephy. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+                            
+    var window: UIWindow?
+
+    
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject : AnyObject]?) -> Bool {
+        
+        
+        
+        Parse.setApplicationId("uBj1Bkw6kPyqfw0HZyyNGQPcpPnimw0UngHWPKhG", clientKey: "VFuMsi7aSPezbeDfyDFzyqFmEC0w1HcvMgQx5YJu")
+        
+        PFFacebookUtils.initializeFacebook()
+        
+        return true
+    }
+
+    // Changed UIApplication! to UIApplication
+    
+    func applicationWillResignActive(application: UIApplication) {
+        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    }
+    
+    // Changed UIApplication! to UIApplication
+    
+    func applicationDidEnterBackground(application: UIApplication) {
+        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    }
+    
+    // Changed UIApplication! to UIApplication
+    
+    func applicationWillEnterForeground(application: UIApplication) {
+        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    }
+    
+    // Changed UIApplication! to UIApplication
+    
+    func applicationWillTerminate(application: UIApplication) {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+    
+    /*  Changed
+
+        func application(application: UIApplication, openURL url: NSURL,
+            sourceApplication: NSString, annotation: AnyObject) -> Bool {
+         
+        to
+
+        func application(application: UIApplication, openURL url: NSURL, sourceApplication: String?, annotation: AnyObject?) -> Bool {
+
+    */
+
+    func application(application: UIApplication, openURL url: NSURL, sourceApplication: String?, annotation: AnyObject?) -> Bool {
+        
+                return FBAppCall.handleOpenURL(url, sourceApplication:sourceApplication,
+                withSession:PFFacebookUtils.session())
+    }
+    
+    func applicationDidBecomeActive(application: UIApplication) {
+        
+         FBAppCall.handleDidBecomeActiveWithSession(PFFacebookUtils.session())
+        
+    }
+    
+
+
+}
+
---
+++ Tinder/Tinder/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  Tinder
+//
+//  Created by David Leonard on 03/25/15.
+//  Copyright (c) 2015 DrkSephy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        var permissions = ["public_profile"]
+        
+        // Update - added , block:
+        
+        PFFacebookUtils.logInWithPermissions(permissions, block: {
+            (user: PFUser!, error: NSError!) -> Void in
+            if user == nil {
+                NSLog("Uh oh. The user cancelled the Facebook login.")
+            } else if user.isNew {
+                NSLog("User signed up and logged in through Facebook!")
+            } else {
+                NSLog("User logged in through Facebook!")
+            }
+        })
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
