92d667dd60a3fc21f106d2ff024ca936c1881976
David Leonard <sephirothcloud1025@yahoo.com>
2015-03-24 23:24:26 -0400

    Implementing image/text posting and storing to Parse backend

--- Instagram/Instagram/postViewController.swift
+++ Instagram/Instagram/postViewController.swift
@@ -10,6 +10,21 @@ import UIKit
 
 class postViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
+    func displayAlert(title:String, error:String) {
+        
+        var alert = UIAlertController(title: title, message: error, preferredStyle: UIAlertControllerStyle.Alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .Default, handler: { action in
+            
+            self.dismissViewControllerAnimated(true, completion: nil)
+            
+        }))
+        
+        self.presentViewController(alert, animated: true, completion: nil)
+        
+    }
+    
+    var photoSelected: Bool = false;
+    
     @IBOutlet weak var imageToPost: UIImageView!
     
     
@@ -25,12 +40,48 @@ class postViewController: UIViewController, UINavigationControllerDelegate, UIIm
     @IBOutlet weak var shareText: UITextField!
     
     @IBAction func postImage(sender: AnyObject) {
+        var error = "";
+        if photoSelected == false {
+            error = "Please select an image to post!";
+        } else if shareText.text == "" {
+            error = "Please enter a message";
+        }
+        
+        if error != "" {
+            displayAlert("Cannot Post Image", error: error);
+        } else {
+            var post = PFObject(className: "Post"); // Store each of our images as "Post" classes
+            // save the text
+            post["Title"] = shareText.text;
+            
+            post.saveInBackgroundWithBlock{(success: Bool!, error: NSError!) -> Void in
+                if success == false {
+                    self.displayAlert("Could not post image!", error: "Please try again later");
+                } else {
+                    let imageData = UIImagePNGRepresentation(self.imageToPost.image);
+                    let imageFile = PFFile(name: "image.png", data: imageData);
+                    post["imageFile"] = imageFile;
+                    
+                    post.saveInBackgroundWithBlock{(success: Bool!, error: NSError!) -> Void in
+                        if success == false {
+                            self.displayAlert("Could not post image!", error: "Please try again later");
+                        } else {
+                            println("Posted successfully!");
+                    }
+                    
+                }
+            }
+            
+            
+            }
+        }
     }
     
     func imagePickerController(picker: UIImagePickerController!, didFinishPickingImage image: UIImage!, editingInfo: [NSObject : AnyObject]!) {
         println("Image Selected");
         self.dismissViewControllerAnimated(true, completion: nil);
         imageToPost.image = image;
+        photoSelected = true;
     }
     
     override func viewDidLoad() {
