20db5d450eb83ba65070b2f0280c9f60e16fee13
David Leonard <sephirothcloud1025@yahoo.com>
2015-03-25 10:50:39 -0400

    Displaying text and image titles from within Parse backend, fixing bug with nil username

--- Instagram/Instagram/feedTableViewController.swift
+++ Instagram/Instagram/feedTableViewController.swift
@@ -10,9 +10,35 @@ import UIKit
 
 class feedTableViewController: UITableViewController {
     
+    var titles = [String]();
+    var usernames = [String]();
+    var images = [UIImage]();
+    var imageFiles = [PFFile]();
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        var query = PFQuery(className:"Post")
+        // query.whereKey("playerName", equalTo:"Sean Plott")
+        query.findObjectsInBackgroundWithBlock {
+            (objects: [AnyObject]!, error: NSError!) -> Void in
+            if error == nil {
+                // The find succeeded.
+                println("Successfully retrieved \(objects.count) scores.")
+                // Do something with the found objects
+                for object in objects {
+                    // println(object["username"]);
+                    self.titles.append(object["Title"] as String);
+                    self.usernames.append(object["username"] as String);
+                    self.imageFiles.append(object["imageFile"] as PFFile);
+                    self.tableView.reloadData();
+                }
+                
+            } else {
+                // Log details of the failure
+                println(error);
+            }
+        }
     
     }
 
@@ -26,18 +52,18 @@ class feedTableViewController: UITableViewController {
     }
     
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3;
+        return titles.count;
     }
     
     override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
-        return 227; 
+        return 227;
     }
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         // We are generating our own custom class with its own properties in order to update each cell at the cell level,
         // as opposed to the page level
         var myCell:cell = self.tableView.dequeueReusableCellWithIdentifier("myCell") as cell;
-        myCell.title.text = "My title";
-        myCell.username.text = "David";
+        myCell.title.text = titles[indexPath.row];
+        myCell.username.text = usernames[indexPath.row];
         myCell.sizeToFit();
         
         return myCell;
