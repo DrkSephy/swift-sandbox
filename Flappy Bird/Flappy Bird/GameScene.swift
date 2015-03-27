e638082c8b1f2301c24564e3ce2fae74ae049eb0
David Leonard <sephirothcloud1025@yahoo.com>
2015-03-26 21:46:27 -0400

    Creating new Sprite Kit App, removing unncessary code

---
+++ Flappy Bird/Flappy Bird/GameScene.swift
@@ -0,0 +1,23 @@
+//
+//  GameScene.swift
+//  Flappy Bird
+//
+//  Created by David Leonard on 3/26/15.
+//  Copyright (c) 2015 David Leonard. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMoveToView(view: SKView) {
+        
+    }
+    
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        /* Called when a touch begins */
+        }
+   
+    override func update(currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+    }
+}
