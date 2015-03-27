8a0cb9fbc33ed3a21cccb751e2a7ead152967c71
David Leonard <sephirothcloud1025@yahoo.com>
2015-03-26 21:05:45 -0400

    Setting up new view controller for adding new users

--- Tinder/SignUpViewController.swift
+++ Tinder/SignUpViewController.swift
@@ -65,14 +65,4 @@ class SignUpViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
